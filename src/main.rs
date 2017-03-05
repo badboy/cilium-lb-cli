6ed838ee06bbc6a050cbac02543f9737093e9043
Jan-Erik Rediger <janerik@fnordig.de>
2017-03-05 11:43:41 +0100

    Proper CLI frontend with subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,59 @@
+extern crate clap;
+#[macro_use]
+extern crate error_chain;
+
+use std::io::Write;
+use clap::{Arg, ArgMatches, App, AppSettings, SubCommand};
+
 mod map;
 mod bpf;
 
 use map::Map;
 
-fn main() {
-    let f = "/sys/fs/bpf/tc/globals/filter_mac";
+error_chain! {
+    foreign_links {
+        Io(::std::io::Error);
+    }
+}
 
-    let map = Map::from_path(f).unwrap();
-    println!("{}", map);
+pub fn report_error(e: Error) {
+    let _ = write!(::std::io::stderr(), "error:");
+    let _ = writeln!(::std::io::stderr(), " {}", e);
+    for e in e.iter().skip(1) {
+        let _ = writeln!(::std::io::stderr(), "   caused by: {}", e);
+    }
+}
+
+fn info<'a>(args: &ArgMatches<'a>) -> Result<()> {
+    let map_path: String = args.value_of_os("MAP_PATH")
+        .expect("TARGET is required")
+        .to_os_string()
+        .into_string().map_err(|_| "MAP_PATH must be valid unicode")?;
+
+    Map::from_path(&map_path).map(|map| {
+        println!("{}", map);
+        ()
+    }).chain_err(|| format!("Failed to parse info about map"))
+}
+
+fn main() {
+    let app = App::new("bpf-map")
+        .version("0.1.0")
+        .author("Jan-Erik Rediger <janerik@fnordig.de>")
+        .about("Inspect persisted BPF maps")
+        .settings(&[AppSettings::SubcommandRequired])
+        .subcommand(SubCommand::with_name("info")
+                    .about("Print metadata information of map")
+                    .arg(Arg::with_name("MAP_PATH").required(true)
+                         .help("name of the fuzz target")));
+
+    let args = app.get_matches();
+
+    ::std::process::exit(match args.subcommand() {
+        ("info", matches) => info(matches.expect("arguments present")),
+        (s, _) => panic!("unimplemented subcommand {}!", s),
+    }.map(|_| 0).unwrap_or_else(|err| {
+        report_error(err);
+        1
+    }));
 }
