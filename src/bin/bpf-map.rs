3c39500b3ba3227833eb0b2ff7d0bd6b7ce24735
Jan-Erik Rediger <janerik@fnordig.de>
2017-03-05 13:06:28 +0100

    Dump contents of map

--- src/bin/bpf-map.rs
+++ src/bin/bpf-map.rs
@@ -22,6 +22,37 @@ pub fn report_error(e: Error) {
     }
 }
 
+fn hex_print(data: &[u8]) {
+    for (idx, row) in data.chunks(16).enumerate() {
+        print!("{:>08x}  ", idx*16);
+        for (pos, c) in row.iter().enumerate() {
+            print!("{:>02x} ", c);
+            if pos == 7 {
+                print!(" ");
+            }
+        }
+        if row.len() < 7 {
+            print!(" ");
+        }
+        let fillup = 16-row.len();
+        for _ in 0..(fillup*3) { print!(" "); }
+        if fillup > 0 { print!(" "); }
+
+        print!(" |");
+        for &c in row {
+            if c >= ' ' as u8 && c <= '~' as u8 {
+                let c = c as char;
+                print!("{}", c);
+            } else {
+                print!(".");
+            }
+        }
+        print!("|");
+
+        println!("");
+    }
+}
+
 fn info<'a>(args: &ArgMatches<'a>) -> Result<()> {
     let map_path: String = args.value_of_os("MAP_PATH")
         .expect("TARGET is required")
@@ -34,6 +65,26 @@ fn info<'a>(args: &ArgMatches<'a>) -> Result<()> {
     }).chain_err(|| format!("Failed to parse info about map"))
 }
 
+fn dump<'a>(args: &ArgMatches<'a>) -> Result<()> {
+    let map_path: String = args.value_of_os("MAP_PATH")
+        .expect("TARGET is required")
+        .to_os_string()
+        .into_string().map_err(|_| "MAP_PATH must be valid unicode")?;
+
+    let map = Map::from_path(&map_path)
+        .chain_err(|| format!("Failed to parse info about map"))?;
+
+    for (key, val) in &map {
+        println!("Key:");
+        hex_print(&key);
+        println!("Value:");
+        hex_print(&val);
+        println!("");
+    }
+
+    Ok(())
+}
+
 fn main() {
     let app = App::new("bpf-map")
         .version("0.1.0")
@@ -43,12 +94,17 @@ fn main() {
         .subcommand(SubCommand::with_name("info")
                     .about("Print metadata information of map")
                     .arg(Arg::with_name("MAP_PATH").required(true)
+                         .help("Path to the map")))
+        .subcommand(SubCommand::with_name("dump")
+                    .about("Dump contents of map")
+                    .arg(Arg::with_name("MAP_PATH").required(true)
                          .help("Path to the map")));
 
     let args = app.get_matches();
 
     ::std::process::exit(match args.subcommand() {
         ("info", matches) => info(matches.expect("arguments present")),
+        ("dump", matches) => dump(matches.expect("arguments present")),
         (s, _) => panic!("unimplemented subcommand {}!", s),
     }.map(|_| 0).unwrap_or_else(|err| {
         report_error(err);
